b6ec180a85a5bea33a5be68c83efdac14f8f99bf
thcmatias <thcmatias@gmail.com>
2015-12-21 16:24:36 -0200

    + Finishing models creation
    -> new models: Viagem and PontoDeVenda

---
+++ src/model/PontoDeVenda.java
@@ -0,0 +1,41 @@
+package model;
+
+public class PontoDeVenda {
+	private String nome;
+	private String endereco;
+	private String telefone;
+	private int codigo;
+	private String cnpj;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+}
---
+++ src/model/Viagem.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Viagem {
+	private ArrayList<Percurso> plano_de_viagem;
+
+	public ArrayList<Percurso> getPlanoDeViagem() {
+		return plano_de_viagem;
+	}
+
+	public void setPlanoDeViagem(ArrayList<Percurso> plano_de_viagem) {
+		this.plano_de_viagem = plano_de_viagem;
+	}
+}
