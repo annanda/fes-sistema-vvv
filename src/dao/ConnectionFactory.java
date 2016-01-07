8dd59ef601cc7bb918baa74d73826f8d76286e9c
thcmatias <thcmatias@gmail.com>
2016-01-07 09:52:09 -0200

    +* refactoring DAO
    -> Now DAO is a class that all ModelDAO extends, with connection and statement parameters and connect() and disconnect() methods;
    -> A new class named ConnectionFactory was made to setup the driver and build new connections for DAO extended classes.

---
+++ src/dao/ConnectionFactory.java
@@ -0,0 +1,29 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	public static Connection createConnection() {
+		Connection connection = null;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager
+					.getConnection("jdbc:mysql://localhost/vvv_db?user=root");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return connection;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
--- src/dao/DAO.java
+++ src/dao/DAO.java
@@ -1,53 +1,39 @@
 package dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DAO {
-	private static Connection connection = null;
-
-	public Connection getConnection() {
-		return connection;
-	}
-
-	public static Connection Connect() {
+	private Connection connection;
+	private Statement statement;
+	
+	protected void connect() {
+		connection = ConnectionFactory.createConnection();
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			connection = DriverManager
-					.getConnection("jdbc:mysql://localhost/vvv_db?user=root");
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		return connection;
-	}
-
-	public static void Disconnect() {
-		try {
-			connection.close();
+			statement = connection.createStatement();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-	// public static void main(String[] args) {
-	// DAO.Connect();
-	// try {
-	// System.out.println(connection.isClosed());
-	// } catch (SQLException e) {
-	// e.printStackTrace();
-	// }
-	// DAO.Disconnect();
-	// try {
-	// System.out.println(connection.isClosed());
-	// } catch (SQLException e) {
-	// e.printStackTrace();
-	// }
-	// }
+	protected void disconnect() {
+		try {
+			if (!connection.isClosed()) {
+				connection.close();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			if (!statement.isClosed()) {
+				statement.close();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
