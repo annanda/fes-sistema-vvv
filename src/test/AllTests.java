2921d4d01762abce498bbd33c9ef182905740059
rodrigozhou <rodrigo.zhou@gmail.com>
2016-01-10 22:01:31 -0200

    fix tests, added new ones

--- src/test/AllTests.java
+++ src/test/AllTests.java
@@ -5,7 +5,11 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TestPessoa.class })
+@SuiteClasses({
+	TestPessoa.class,
+	TestUsuario.class,
+	TestCidade.class
+})
 public class AllTests {
 
 }
---
+++ src/test/TestCidade.java
@@ -0,0 +1,60 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import controller.CidadeController;
+import model.Cidade;
+
+public class TestCidade {
+
+	@Test
+	public void cadastrarCidade() {
+		int id = 0;
+		try {
+			id = CidadeController.cadastrarCidade("test", "tes", "test");;
+		} catch (Exception e) {
+			fail();
+		} finally {
+			if (id > 0)
+				CidadeController.deletarCidade(id);
+		}
+	}
+
+	@Test
+	public void listarCidades() {
+		int id = CidadeController.cadastrarCidade("test", "tes", "test");
+		ArrayList<Cidade> l = CidadeController.listarCidades("test", "tes", "test");
+		CidadeController.deletarCidade(id);
+		assertNotNull(l);
+		assertEquals(l.size(), 1);
+	}
+
+	@Test
+	public void alterarCidade() {
+		int id = 0;
+		try {
+			id = CidadeController.cadastrarCidade("test", "tes", "test");
+			CidadeController.alterarCidade(id, "asdf", "asd", "asdf");
+		} catch (Exception e) {
+			fail();
+		} finally {
+			if (id > 0)
+				CidadeController.deletarCidade(id);
+		}
+	}
+
+	@Test
+	public void deletarCidade() {
+		try {
+			int id = CidadeController.cadastrarCidade("test", "tes", "test");
+			CidadeController.deletarCidade(id);
+		} catch (Exception e) {
+			fail();
+		}
+	}
+
+}
