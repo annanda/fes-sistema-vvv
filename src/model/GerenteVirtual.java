1fb0ee13a54fe5f125acb230f0bfeba0b0a2f4a4
thcmatias <thcmatias@gmail.com>
2016-01-31 20:58:18 -0200

    * just doing...

--- src/model/GerenteVirtual.java
+++ src/model/GerenteVirtual.java
@@ -2,12 +2,11 @@ package model;
 
 public class GerenteVirtual extends Usuario {
     // CONSTRUCTOR
-    public GerenteVirtual(String nome, String endereco, String codigo, String email, String senha,
-            int nivel_permissao) {
-        super(nome, endereco, codigo, email, senha, nivel_permissao);
+    public GerenteVirtual(String nome, String endereco, String codigo, String email, String senha) {
+        super(nome, endereco, codigo, email, senha, 2);
     }
 
     public void aprovarVendas() {
-
+        
     }
 }
