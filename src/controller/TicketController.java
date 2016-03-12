6866c9658c2356dc3f65c4fe6af40757bb0a4b90
thcmatias <thacmatias@gmail.com>
2016-03-11 22:35:01 -0300

    * implementing TicketController and TicketDAO

--- src/controller/TicketController.java
+++ src/controller/TicketController.java
@@ -1,5 +1,56 @@
 package controller;
 
-public class TicketController {
+import java.util.ArrayList;
+import java.util.HashMap;
 
+import model.Ticket;
+import dao.TicketDAO;
+
+public class TicketController {
+    public static int cadastrarTicket(int id_reserva, int id_percurso, int numero,
+            String id_localizador) {
+        int id = 0;
+        TicketDAO ticket_dao = new TicketDAO();
+        Ticket novo_ticket = getTicketByIdLocalizador(id_localizador);
+
+        if (novo_ticket == null) {
+            novo_ticket =
+                    new Ticket(numero, id_localizador,
+                            ReservaController.getReservaById(id_reserva),
+                            PercursoController.getPercursoById(id_percurso));
+            id = ticket_dao.cadastrarTicket(novo_ticket);
+        } else {
+            System.out
+                    .println("Id Localizador ja cadastrado. Ticket pode ja ter sido cadastrado no sistema");
+        }
+
+        return id;
+    }
+
+    public static ArrayList<Ticket> listarTickets(int id_reserva, int id_percurso, int numero) {
+        TicketDAO ticket_dao = new TicketDAO();
+        HashMap<String, String> conditions = new HashMap<String, String>();
+
+        conditions.put("id_reserva", "" + id_reserva);
+        conditions.put("id_percurso", "" + id_percurso);
+        conditions.put("numero", "" + numero);
+
+        return ticket_dao.listarTickets(conditions);
+    }
+
+    public static Ticket getTicketById(int id) {
+        TicketDAO ticket_dao = new TicketDAO();
+
+        return ticket_dao.getTicketById(id);
+    }
+
+    public static Ticket getTicketByIdLocalizador(String id_localizador) {
+        TicketDAO ticket_dao = new TicketDAO();
+
+        return ticket_dao.getTicketByIdLocalizador(id_localizador);
+    }
+
+    // Tickets nao podem ser alterados
+
+    // Tickets nao podem ser deletados
 }
--- src/dao/TicketDAO.java
+++ src/dao/TicketDAO.java
@@ -1,5 +1,120 @@
 package dao;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import controller.PercursoController;
+import controller.ReservaController;
+import model.Constants;
+import model.Ticket;
+
 public class TicketDAO extends DAO {
+    final private String tabela = "tickets";
+
+    public int cadastrarTicket(Ticket novo_ticket) {
+        final String id_ticket_label = "id_ticket";
+        int id = 0;
+        String id_localizador = novo_ticket.getIdLocalizador();
+        String sql_query =
+                insertFactory(tabela, new String[] { "" + novo_ticket.getReserva().getId(),
+                        "" + novo_ticket.getPercurso().getId(), "" + novo_ticket.getNumero(),
+                        novo_ticket.getIdLocalizador() });
+
+        connect();
+        try {
+            statement.executeUpdate(sql_query);
+            sql_query =
+                    selectFactory(tabela, new String[] { id_ticket_label }, "id_localizador = '"
+                            + id_localizador + Constants.SINGLE_QUOTE);
+            result_set = statement.executeQuery(sql_query);
+            if (result_set.first()) {
+                id = result_set.getInt(id_ticket_label);
+            }
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        disconnect();
+
+        return id;
+    }
+
+    public ArrayList<Ticket> listarTickets(HashMap<String, String> conditions) {
+        String sql_query =
+                selectFactory(tabela, new String[] { Constants.ASTERISK }, likeFactory(conditions));
+        ArrayList<Ticket> tickets_encontrados = new ArrayList<Ticket>();
+
+        connect();
+        try {
+            result_set = statement.executeQuery(sql_query);
+            while (result_set.next()) {
+                int id_ticket = result_set.getInt("id_ticket");
+                tickets_encontrados.add(new Ticket(id_ticket, result_set.getInt("numero"),
+                        result_set.getString("id_localizador"), ReservaController
+                                .getReservaById(result_set.getInt("id_reserva")),
+                        PercursoController.getPercursoById(result_set.getInt("id_percurso"))));
+            }
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        disconnect();
+
+        return tickets_encontrados;
+    }
+
+    public Ticket getTicketById(int id) {
+        Ticket ticket_encontrado = null;
+        String sql_query =
+                selectFactory(tabela, new String[] { Constants.ASTERISK }, "id_ticket = " + id);
+
+        connect();
+        try {
+            result_set = statement.executeQuery(sql_query);
+            if (result_set.first()) {
+                ticket_encontrado =
+                        new Ticket(
+                                result_set.getInt("id_ticket"),
+                                result_set.getInt("numero"),
+                                result_set.getString("id_localizador"),
+                                ReservaController.getReservaById(result_set.getInt("id_reserva")),
+                                PercursoController.getPercursoById(result_set.getInt("id_percurso")));
+            }
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        disconnect();
+
+        return ticket_encontrado;
+    }
+
+    public Ticket getTicketByIdLocalizador(String id_localizador) {
+        Ticket ticket_encontrado = null;
+        String sql_query =
+                selectFactory(tabela, new String[] { Constants.ASTERISK }, "id_localizador = '"
+                        + id_localizador + Constants.SINGLE_QUOTE);
+
+        connect();
+        try {
+            result_set = statement.executeQuery(sql_query);
+            if (result_set.first()) {
+                ticket_encontrado =
+                        new Ticket(
+                                result_set.getInt("id_ticket"),
+                                result_set.getInt("numero"),
+                                result_set.getString("id_localizador"),
+                                ReservaController.getReservaById(result_set.getInt("id_reserva")),
+                                PercursoController.getPercursoById(result_set.getInt("id_percurso")));
+            }
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        disconnect();
+
+        return ticket_encontrado;
+    }
 
 }
