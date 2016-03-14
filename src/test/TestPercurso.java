9198e9acb1d71f76ceaea368b2a1633487b7d53f
thcmatias <thacmatias@gmail.com>
2016-03-14 15:39:09 -0300

    * implementing TestPercurso and TestTicket

--- src/test/TestPercurso.java
+++ src/test/TestPercurso.java
@@ -4,8 +4,89 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 
-import org.junit.Test;
+import model.Cidade;
+import model.Percurso;
+
+import org.junit.*;
+
+import controller.CidadeController;
+import controller.PercursoController;
 
 public class TestPercurso {
+    Cidade partida, chegada;
+
+    @Before
+    public void init() {
+        partida =
+                CidadeController.getCidadeById(CidadeController.cadastrarCidade("Rio de Janeiro",
+                        "RDJ", "222"));
+        chegada =
+                CidadeController.getCidadeById(CidadeController.cadastrarCidade("Sao Paulo", "SPA",
+                        "paradepois"));
+    }
+
+    @After
+    public void finish() {
+        CidadeController.deletarCidade(partida.getId());
+        CidadeController.deletarCidade(chegada.getId());
+    }
+
+    @Test
+    public void cadastrarPercurso() {
+        int id = 0;
+        try {
+            id =
+                    PercursoController.cadastrarPercurso("0", partida.getCodigo(),
+                            chegada.getCodigo(), "2012-12-21 12:21:12", "5", "");
+        } catch (Exception e) {
+            fail();
+        } finally {
+            if (id > 0) {
+                PercursoController.deletarPercurso(id);
+            }
+        }
+    }
+
+    @Test
+    public void listarPercursos() {
+        int id =
+                PercursoController.cadastrarPercurso("0", partida.getCodigo(), chegada.getCodigo(),
+                        "2012-12-21 12:21:12", "5", "");
+        ArrayList<Percurso> l =
+                PercursoController.listarPercursos("" + partida.getId(), "" + chegada.getId(),
+                        "2012-12-21 12:21:12", "");
+        PercursoController.deletarPercurso(id);
+        assertNotNull(l);
+        assertEquals(l.size(), 1);
+    }
+
+    @Test
+    public void alterarPercursos() {
+        int id = 0;
+        try {
+            id =
+                    PercursoController.cadastrarPercurso("0", partida.getCodigo(),
+                            chegada.getCodigo(), "2012-12-21 12:21:12", "5", "");
+            PercursoController.alterarPercurso(id, "0", "1", "aeroporto123");
+        } catch (Exception e) {
+            fail();
+        } finally {
+            if (id > 0) {
+                PercursoController.deletarPercurso(id);
+            }
+        }
+    }
+
+    @Test
+    public void deletarPercurso() {
+        try {
+            int id =
+                    PercursoController.cadastrarPercurso("0", partida.getCodigo(),
+                            chegada.getCodigo(), "2012-12-21 12:21:12", "5", "");
+            PercursoController.deletarPercurso(id);
+        } catch (Exception e) {
+            fail();
+        }
+    }
 
 }
--- src/test/TestTicket.java
+++ src/test/TestTicket.java
@@ -19,33 +19,16 @@ import controller.TicketController;
 
 public class TestTicket {
 
-    Reserva temp_reserva;
-    Percurso temp_percurso;
-    String[] temp_ids_passageiros;
+    int temp_id_reserva, temp_id_percurso, temp_id_modal, temp_id_usuario, temp_id_viagem;
+    String[] temp_ids_passageiros, temp_ids_percursos;
 
     @Before
     public void init() {
-        int i = 0;
-        temp_ids_passageiros = new String[5];
-        for (String temp_id_passageiro : temp_ids_passageiros) {
-            String temp_cpf = (i % 2 > 0) ? "0000000000" + (i - 1) : "";
-            temp_id_passageiro =
-                    ""
-                            + PassageiroController.cadastrarPassageiro("fulano" + i,
-                                    "endereco" + i, "codigo" + i, "0000000000" + i, "3456789" + i,
-                                    "profissao" + i, "10-03-199" + (8 + i), temp_cpf);
-            i++;
-        }
-        temp_reserva =
-                ReservaController.cadastrarReserva("abc", 3, "cartao", 333.1234, true,
-                        ids_passageiros);
     }
 
     @Test
     public void cadastrarTicket() {
-        int id = 0;
         try {
-            id = TicketController.cadastrarTicket(id_reserva, id_percurso, numero, id_localizador);
         } catch (Exception e) {
             // TODO: handle exception
         } finally {
